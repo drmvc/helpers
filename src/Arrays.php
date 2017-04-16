c793d3b15de7d33b02db8fb82647ee6d835406cf
Paul Rock <paul@drteam.rocks>
2017-04-17 00:41:33 +0300

    few new methods and all tests

--- src/Arrays.php
+++ src/Arrays.php
@@ -4,6 +4,44 @@ class Arrays
 {
 
     /**
+     * Check if array is multidimensional
+     *
+     * @param array $array
+     * @return bool
+     */
+    static function is_md_array($array)
+    {
+        // First we need know what value is array
+        if (is_array($array))
+            // Next read cols from array
+            foreach ($array as $value)
+                // And if some element is array then $array is multidimensional
+                if (is_array($value))
+                    return true;
+
+        return false;
+    }
+
+    /**
+     * Check if two arrays is equal, with same keys
+     *
+     * @param array $a
+     * @param array $b
+     * @return bool
+     */
+    static function array_equal($a, $b)
+    {
+        // Both arrays should be... arrays
+        $is_arrays = (is_array($a) && is_array($b));
+        // Elements count should be equal
+        $count_equal = (count($a) == count($b));
+        // Between the keys should not be differences
+        $difference = (array_diff($a, $b) === array_diff($b, $a));
+
+        return ($is_arrays && $count_equal && $difference);
+    }
+
+    /**
      * Read any files and folder into some directory
      *
      * @param string $path - Source directory with files
@@ -25,39 +63,41 @@ class Arrays
     }
 
     /**
-     * Multidimensional search
+     * Find nested array inside two-dimensional array
      *
-     * @param $parents
-     * @param $searched
-     * @param $object
-     * @return bool|int|string
+     * @param array|object $multidimensional - Where need to find
+     * @param array|object $target - What we need to find
+     * @param bool $object - Method work mode, if object then true, if array then false
+     * @return bool|array
      */
-    static function md_search($parents, $searched, $object = true)
+    static function md_search($multidimensional, $target, $object = true)
     {
-        if (empty($searched) || empty($parents)) {
-            return false;
-        }
+        if (empty($target) || empty($multidimensional)) return false;
 
-        $for_out = null;
-        foreach ($parents as $key => $value) {
+        // Default state for output value
+        $for_out = array();
+
+        // Parse the $multidimensional array by keys and values
+        foreach ($multidimensional as $key => $value) {
+            //error_log($key);
+            //error_log(print_r($value));
             $exists = true;
 
-            foreach ($searched as $skey => $svalue) {
-                if ($object)
-                    $exists = ($exists && isset($value->$skey) && ($value->$skey == $svalue));
-                else
-                    $exists = ($exists && isset($value[$skey]) && ($value[$skey] == $svalue));
+            foreach ($target as $skey => $svalue) {
+                //error_log($skey . '=>' . $svalue);
+                if ($object) $exists = ($exists && isset($value->$skey) && ($value->$skey == $svalue));
+                else $exists = ($exists && isset($value[$skey]) && ($value[$skey] == $svalue));
             }
 
-            if ($exists) {
-                $for_out[] = $key;
-            }
+            // Generate array of keys with founded subArrays
+            if ($exists) $for_out[] = $key;
         }
-        if (!empty($for_out)) {
-            return $for_out;
-        } else {
-            return false;
-        }
+
+        // If output is not empty
+        if (!empty($for_out)) return $for_out;
+
+        // False by default
+        return false;
     }
 
 }
--- tests/Arrays.php
+++ tests/Arrays.php
@@ -6,9 +6,64 @@ use DrMVC\Helpers\Arrays;
 
 class ArraysTest extends TestCase
 {
-    public function testDefault()
+    public $array;
+    public $dir;
+    public $dir_array;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
-        return true;
+        parent::__construct($name, $data, $dataName);
+
+        $this->array[1] = array('k1' => 'v1', 'k2' => 'v2');
+        $this->array[2] = array('k2' => 'v2', 'k1' => 'v1');
+        $this->array[3] = array('k1' => 'v1', 'k3' => 'v3');
+
+        $this->dir = __DIR__ . '/dir4tests';
+        $this->dir_array = array(
+            'dummy.txt',
+            'subdirectory1' => array(
+                'dummy1.txt',
+                'dummy2.txt'
+            ),
+            'subdirectory2' => array(
+                'dummy.txt',
+                'subdirectory3' => array(
+                    'dummynator.txt'
+                )
+            )
+        );
     }
 
+    public function testIsMDArray()
+    {
+        $this->assertTrue(Arrays::is_md_array($this->array));
+        $this->assertFalse(Arrays::is_md_array($this->array[1]));
+    }
+
+    public function testArrayEqual()
+    {
+        $this->assertTrue(Arrays::array_equal($this->array[1], $this->array[1]));
+        $this->assertTrue(Arrays::array_equal($this->array[1], $this->array[2]));
+        $this->assertFalse(Arrays::array_equal($this->array[1], $this->array[3]));
+    }
+
+    public function testDirToArray()
+    {
+        $files = Arrays::dir_to_array($this->dir);
+        $files_tree = print_r($files, true);
+        $dir_tree = print_r($this->dir_array, true);
+        $dir_tree2 = print_r($this->dir_array['subdirectory2'], true);
+
+        $this->assertTrue($files_tree == $dir_tree);
+        $this->assertFalse($files_tree == $dir_tree2);
+    }
+
+    public function testMDSearch()
+    {
+        $result1 = Arrays::md_search($this->array, $this->array[3], false);
+        $result2 = Arrays::md_search($this->array, array('some' => 'value'), false);
+        error_log($result2);
+        $this->assertTrue($result1[0] == 3);
+        $this->assertFalse($result2);
+    }
 }
